2b906df591a7fc056c899142f0d6e3fe55115167
buihang <camhang190497@gmail.com>
2022-01-06 19:53:25 +0700

    React Animation Components

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -4,10 +4,16 @@ import { Link } from 'react-router-dom';
 import { Control, LocalForm } from 'react-redux-form';
 import { Loading } from './LoadingComponent';
 import { baseUrl } from '../shared/baseUrl';
+import { FadeTransform, Fade, Stagger } from 'react-animation-components';
     
     function RenderDish({dish}) {
         return(
             <div className="col-12 col-md-5 m-1">
+                <FadeTransform
+                in
+                transformProps={{
+                    exitTransform: 'scale(0.5) translateY(-50%)'
+                }}>
             
                 <Card>
                     <CardImg width="100%" src={baseUrl + dish.image} alt={dish.name}/>
@@ -16,6 +22,7 @@ import { baseUrl } from '../shared/baseUrl';
                     <CardText>{dish.description}</CardText>
                     </CardBody>
                 </Card>
+                </FadeTransform>
             </div>
         )
     }
@@ -25,15 +32,19 @@ import { baseUrl } from '../shared/baseUrl';
             <div className="col-12 col-md-5 m-1">
                 <h4>Comments</h4>
                 <ul className="list-unstyle">
+                <Stagger in>
                     {comments.map((comment) =>{
                         return (
+                            <Fade in>
                             <li key={comment.id}>
                             <p>{comment.comment}</p>
                             <p>--{comment.author},{new Intl.DateTimeFormat('en-US', { year: 'numeric', month: 'short', day:'2-digit'}).format(new Date(Date.parse(comment.date)))}</p>
                             </li>
+                            </Fade>
 
-                        )
+                        );
                     })}
+                </Stagger>
                 </ul>
                 <CommentForm dishId={dishId} postComment={postComment}/>
 
